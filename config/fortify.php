d3c88e80df6ec2ecf0ee931e2bea26b6388d758d
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-02-11 13:03:03 +0100

    ✨ sanctum allow registration

--- config/fortify.php
+++ config/fortify.php
@@ -130,7 +130,7 @@ return [
     */
 
     'features' => [
-        // Features::registration(),
+        Features::registration(),
         Features::resetPasswords(),
         // Features::emailVerification(),
         Features::updateProfileInformation(),
