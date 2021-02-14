efd5652e93c362c6d774fecb28a568d9a1597f04
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-02-14 17:37:52 +0100

    💄 add recaptcha validate

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -16,7 +16,7 @@ class ContactController extends Controller
             'name'                  => 'required|string',
             'email'                 => 'required|email',
             'message'               => 'required|string',
-            // 'g-recaptcha-response' => 'required|recaptcha',
+            'g-recaptcha-response'  => 'required|recaptcha',
         ]);
 
         $email = Contact::create([
