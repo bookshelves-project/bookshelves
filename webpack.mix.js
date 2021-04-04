470602b8474f45d52e5b78ac1d0eb682f39467eb
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-04-04 16:21:56 +0200

    🐛 fix

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require("laravel-mix");
 
 /*
  |--------------------------------------------------------------------------
@@ -11,13 +11,14 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js').vue()
-    .postCss('resources/css/app.css', 'public/css', [
-        require('postcss-import'),
-        require('tailwindcss'),
-    ])
-    .webpackConfig(require('./webpack.config'));
+mix
+  .js("resources/js/app.js", "public/js")
+  .postCss("resources/css/app.css", "public/css", [
+    require("postcss-import"),
+    require("tailwindcss"),
+  ])
+  .webpackConfig(require("./webpack.config"));
 
 if (mix.inProduction()) {
-    mix.version();
+  mix.version();
 }
