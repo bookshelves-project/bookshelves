525f3aed5ac084c7c7ce6b76a1eabe29dc1adf01
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-04-05 09:53:25 +0200

    add tailwind jit

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,8 @@ const mix = require("laravel-mix");
 mix
   .js("resources/js/app.js", "public/js")
   .postCss("resources/css/app.css", "public/css", [
+    require("@tailwindcss/jit"),
     require("postcss-import"),
-    require("tailwindcss"),
   ])
   .webpackConfig(require("./webpack.config"));
 
