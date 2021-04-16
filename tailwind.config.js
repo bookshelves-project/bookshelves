acbb8fca1fdada440899ece2aa209136f9b448da
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-04-16 10:12:02 +0200

    rollback

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,66 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-    purge: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
+  purge: [
+    "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+    "./storage/framework/views/*.php",
+    "./resources/views/**/*.blade.php",
+    "./resources/js/**/*.vue",
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  theme: {
+    container: {
+      center: true,
     },
-
-    variants: {
-        extend: {
-            opacity: ['disabled'],
-        },
+    screens: {
+      sm: "360px",
+      md: "600px",
+      lg: "900px",
+      xl: "1300px",
+      "2xl": "1536px",
+      "3xl": "1920px",
     },
-
-    plugins: [require('@tailwindcss/forms')],
+    extend: {
+      colors: {
+        primary: {
+          100: "#e2e0ff",
+          200: "#c4c1ff",
+          300: "#a7a1ff",
+          400: "#8982ff",
+          500: "#6c63ff",
+          600: "#564fcc",
+          700: "#413b99",
+          800: "#2b2866",
+          900: "#161433",
+        },
+      },
+      fontFamily: {
+        quicksand: ["Quicksand"],
+        handlee: ["Handlee"],
+      },
+      height: {
+        hero: "32rem",
+      },
+      maxWidth: {
+        extra: "120rem",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      width: ["hover", "focus"],
+      textColor: ["responsive", "hover", "focus", "group-hover"],
+      scale: ["responsive", "hover", "focus", "active", "group-hover"],
+      translate: ["responsive", "hover", "focus", "active", "group-hover"],
+      backgroundColor: ["hover", "focus", "group-hover"],
+      borderColor: ["hover", "focus", "group-hover"],
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+    require("tailwindcss-debug-screens"),
+  ],
 };
