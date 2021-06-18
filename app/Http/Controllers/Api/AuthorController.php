61e97cd474f97c6a316c64fe41a5e965bdfcb23a
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-06-18 18:07:31 +0200

    fix download controller, add all option for entities

--- app/Http/Controllers/Api/AuthorController.php
+++ app/Http/Controllers/Api/AuthorController.php
@@ -2,135 +2,143 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Models\Author;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Cache;
+use App\Http\Resources\Author\AuthorLightResource;
 use App\Http\Resources\Author\AuthorResource;
+use App\Http\Resources\Author\AuthorUltraLightResource;
 use App\Http\Resources\Book\BookLightResource;
 use App\Http\Resources\Serie\SerieLightResource;
-use App\Http\Resources\Author\AuthorLightResource;
+use App\Models\Author;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class AuthorController extends Controller
 {
-    /**
-     * @OA\Get(
-     *     path="/authors",
-     *     tags={"authors"},
-     *     summary="List of authors",
-     *     description="Authors",
-     *     @OA\Parameter(
-     *         name="perPage",
-     *         in="query",
-     *         description="Integer to choose how many books you show in each page",
-     *         required=false,
-     *         example=32,
-     *         @OA\Schema(
-     *           type="integer",
-     *           format="int64"
-     *         ),
-     *         style="form"
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Successful operation"
-     *     )
-     * )
-     */
-    public function index(Request $request)
-    {
-        $perPage = $request->get('per-page');
-        $perPage = $perPage ? $perPage : 32;
-        if (! is_numeric($perPage)) {
-            return response()->json(
-                "Invalid 'per-page' query parameter, must be an int",
-                400
-            );
-        }
-        $perPage = intval($perPage);
+	/**
+	 * @OA\Get(
+	 *     path="/authors",
+	 *     tags={"authors"},
+	 *     summary="List of authors",
+	 *     description="Authors",
+	 *     @OA\Parameter(
+	 *         name="perPage",
+	 *         in="query",
+	 *         description="Integer to choose how many books you show in each page",
+	 *         required=false,
+	 *         example=32,
+	 *         @OA\Schema(
+	 *           type="integer",
+	 *           format="int64"
+	 *         ),
+	 *         style="form"
+	 *     ),
+	 *     @OA\Response(
+	 *         response=200,
+	 *         description="Successful operation"
+	 *     )
+	 * )
+	 */
+	public function index(Request $request)
+	{
+		$perPage = $request->get('per-page');
+		$perPage = $perPage ? $perPage : 32;
+		if (!is_numeric($perPage)) {
+			return response()->json(
+				"Invalid 'per-page' query parameter, must be an int",
+				400
+			);
+		}
+		$perPage = intval($perPage);
 
-        $cachedAuthors = Cache::get('authors');
+		$all = $request->get('all') ? filter_var($request->get('all'), FILTER_VALIDATE_BOOLEAN) : null;
+		if ($all) {
+			$authors = Author::orderBy('title_sort')->get();
 
-        if (! $cachedAuthors) {
-            $authors = Author::with('media')->orderBy('lastname')->withCount('books')->paginate($perPage);
+			return AuthorUltraLightResource::collection($authors);
+		}
 
-            Cache::remember('authors', 86400, function () use ($authors) {
-                return $authors;
-            });
-        } else {
-            $authors = $cachedAuthors;
-        }
+		$cachedAuthors = Cache::get('authors');
 
-        return AuthorLightResource::collection($authors);
-    }
+		if (!$cachedAuthors) {
+			$authors = Author::with('media')->orderBy('lastname')->withCount('books')->paginate($perPage);
 
-    /**
-     * @OA\Get(
-     *     path="/authors/{author-slug}",
-     *     summary="Show author by author slug",
-     *     tags={"authors"},
-     *     description="Get details for a single author with list of series and books, check /authors endpoint to get list of slugs",
-     *     operationId="findAuthorByAuthorSlug",
-     *     @OA\Parameter(
-     *         name="author-slug",
-     *         in="path",
-     *         description="Slug of author name like 'auel-jean-m' for Jean M. Auel",
-     *         required=true,
-     *         example="auel-jean-m",
-     *         @OA\Schema(
-     *           type="string",
-     *           @OA\Items(type="string"),
-     *         ),
-     *         style="form"
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="successful operation",
-     *         @OA\Schema(ref="#/components/schemas/ApiResponse")
-     *     ),
-     *     @OA\Response(
-     *         response="404",
-     *         description="Invalid author-slug value or book-slug value",
-     *         @OA\Schema(ref="#/components/schemas/ApiResponse")
-     *     ),
-     * )
-     */
-    public function show(string $author)
-    {
-        $author = Author::whereSlug($author)->with('media')->withCount('books')->firstOrFail();
-        $author = AuthorResource::make($author);
+			Cache::remember('authors', 86400, function () use ($authors) {
+				return $authors;
+			});
+		} else {
+			$authors = $cachedAuthors;
+		}
 
-        return $author;
-    }
+		return AuthorLightResource::collection($authors);
+	}
 
-    public function books(string $author)
-    {
-        $author = Author::whereSlug($author)->with(['books.media', 'books.authors', 'books.serie', 'books.language'])->firstOrFail();
+	/**
+	 * @OA\Get(
+	 *     path="/authors/{author-slug}",
+	 *     summary="Show author by author slug",
+	 *     tags={"authors"},
+	 *     description="Get details for a single author with list of series and books, check /authors endpoint to get list of slugs",
+	 *     operationId="findAuthorByAuthorSlug",
+	 *     @OA\Parameter(
+	 *         name="author-slug",
+	 *         in="path",
+	 *         description="Slug of author name like 'auel-jean-m' for Jean M. Auel",
+	 *         required=true,
+	 *         example="auel-jean-m",
+	 *         @OA\Schema(
+	 *           type="string",
+	 *           @OA\Items(type="string"),
+	 *         ),
+	 *         style="form"
+	 *     ),
+	 *     @OA\Response(
+	 *         response=200,
+	 *         description="successful operation",
+	 *         @OA\Schema(ref="#/components/schemas/ApiResponse")
+	 *     ),
+	 *     @OA\Response(
+	 *         response="404",
+	 *         description="Invalid author-slug value or book-slug value",
+	 *         @OA\Schema(ref="#/components/schemas/ApiResponse")
+	 *     ),
+	 * )
+	 */
+	public function show(string $author)
+	{
+		$author = Author::whereSlug($author)->with('media')->withCount('books')->firstOrFail();
+		$author = AuthorResource::make($author);
 
-        return BookLightResource::collection($author->books);
-    }
+		return $author;
+	}
 
-    public function series(string $author)
-    {
-        $author = Author::whereSlug($author)->with(['series.media', 'series.authors', 'series.language'])->firstOrFail();
+	public function books(string $author)
+	{
+		$author = Author::whereSlug($author)->with(['books.media', 'books.authors', 'books.serie', 'books.language'])->firstOrFail();
 
-        return SerieLightResource::collection($author->series);
-    }
+		return BookLightResource::collection($author->books);
+	}
 
-    /**
-     * @OA\Get(
-     *     path="/authors/count",
-     *     tags={"authors"},
-     *     summary="Count for authors",
-     *     description="Count authors",
-     *     @OA\Response(
-     *         response=200,
-     *         description="Successful operation"
-     *     )
-     * )
-     */
-    public function count()
-    {
-        return Author::count();
-    }
+	public function series(string $author)
+	{
+		$author = Author::whereSlug($author)->with(['series.media', 'series.authors', 'series.language'])->firstOrFail();
+
+		return SerieLightResource::collection($author->series);
+	}
+
+	/**
+	 * @OA\Get(
+	 *     path="/authors/count",
+	 *     tags={"authors"},
+	 *     summary="Count for authors",
+	 *     description="Count authors",
+	 *     @OA\Response(
+	 *         response=200,
+	 *         description="Successful operation"
+	 *     )
+	 * )
+	 */
+	public function count()
+	{
+		return Author::count();
+	}
 }
--- app/Http/Controllers/Api/DownloadController.php
+++ app/Http/Controllers/Api/DownloadController.php
@@ -2,215 +2,215 @@
 
 namespace App\Http\Controllers\Api;
 
-use File;
-use ZipArchive;
+use App\Http\Controllers\Controller;
+use App\Models\Author;
 use App\Models\Book;
 use App\Models\Serie;
-use App\Models\Author;
-use Illuminate\Support\Str;
+use File;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
 use Spatie\MediaLibrary\Support\MediaStream;
+use ZipArchive;
 
 class DownloadController extends Controller
 {
-    /**
-     * @OA\Get(
-     *     path="/download/book/{author-slug}/{book-slug}",
-     *     tags={"download"},
-     *     summary="Download specific book",
-     *     description="Download specific book, check /books endpoint to get list of slugs",
-     *     @OA\Parameter(
-     *         name="author-slug",
-     *         in="path",
-     *         description="Slug of author name like 'auel-jean-m' for Jean M. Auel",
-     *         required=true,
-     *         example="auel-jean-m",
-     *         @OA\Schema(
-     *           type="string",
-     *           @OA\Items(type="string"),
-     *         ),
-     *         style="form"
-     *     ),
-     *     @OA\Parameter(
-     *         name="book-slug",
-     *         in="path",
-     *         description="Slug of book name like 'les-refuges-de-pierre' for Les refuges de pierre",
-     *         required=true,
-     *         example="les-refuges-de-pierre",
-     *         @OA\Schema(
-     *           type="string",
-     *           @OA\Items(type="string"),
-     *         ),
-     *         style="form"
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Successful operation"
-     *     )
-     * )
-     */
-    public function book(Request $request, string $author, string $book)
-    {
-        $book = Book::whereSlug($book)->firstOrFail();
-        if ($book->author->slug === $author) {
-            $epub = $book->getMedia('epubs')->first();
-        } else {
-            return response()->json(['Error' => 'Book not found'], 401);
-        }
+	/**
+	 * @OA\Get(
+	 *     path="/download/book/{author-slug}/{book-slug}",
+	 *     tags={"download"},
+	 *     summary="Download specific book",
+	 *     description="Download specific book, check /books endpoint to get list of slugs",
+	 *     @OA\Parameter(
+	 *         name="author-slug",
+	 *         in="path",
+	 *         description="Slug of author name like 'auel-jean-m' for Jean M. Auel",
+	 *         required=true,
+	 *         example="auel-jean-m",
+	 *         @OA\Schema(
+	 *           type="string",
+	 *           @OA\Items(type="string"),
+	 *         ),
+	 *         style="form"
+	 *     ),
+	 *     @OA\Parameter(
+	 *         name="book-slug",
+	 *         in="path",
+	 *         description="Slug of book name like 'les-refuges-de-pierre' for Les refuges de pierre",
+	 *         required=true,
+	 *         example="les-refuges-de-pierre",
+	 *         @OA\Schema(
+	 *           type="string",
+	 *           @OA\Items(type="string"),
+	 *         ),
+	 *         style="form"
+	 *     ),
+	 *     @OA\Response(
+	 *         response=200,
+	 *         description="Successful operation"
+	 *     )
+	 * )
+	 */
+	public function book(Request $request, string $author, string $book)
+	{
+		$book = Book::whereSlug($book)->firstOrFail();
+		if ($book->meta_author === $author) {
+			$epub = $book->getMedia('epubs')->first();
+		} else {
+			return response()->json(['Error' => 'Book not found'], 401);
+		}
 
-        return response()->download($epub->getPath(), $epub->file_name);
-    }
+		return response()->download($epub->getPath(), $epub->file_name);
+	}
 
-    /**
-     * @OA\Get(
-     *     path="/download/serie/{author-slug}/{serie-slug}",
-     *     tags={"download"},
-     *     summary="Download specific serie's books with ZIP",
-     *     description="Download specific serie, check /series endpoint to get list of slugs",
-     *     @OA\Parameter(
-     *         name="author-slug",
-     *         in="path",
-     *         description="Slug of author name like 'auel-jean-m' for Jean M. Auel",
-     *         required=true,
-     *         example="auel-jean-m",
-     *         @OA\Schema(
-     *           type="string",
-     *           @OA\Items(type="string"),
-     *         ),
-     *         style="form"
-     *     ),
-     *     @OA\Parameter(
-     *         name="book-slug",
-     *         in="path",
-     *         description="Slug of book name like 'les-enfants-de-la-terre' for Les enfants de la terre",
-     *         required=true,
-     *         example="les-enfants-de-la-terre",
-     *         @OA\Schema(
-     *           type="string",
-     *           @OA\Items(type="string"),
-     *         ),
-     *         style="form"
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Successful operation"
-     *     )
-     * )
-     */
-    public function serie(string $author, string $serie)
-    {
-        $epubs = [];
-        $author = Author::whereSlug($author)->firstOrFail();
-        $serie = Serie::with('books')->whereSlug($serie)->firstOrFail();
-        $authorFound = false;
-        foreach ($serie->authors as $key => $authorList) {
-            if ($author->slug === $authorList->slug) {
-                $authorFound = true;
-            }
-        }
-        if (! $authorFound) {
-            return response(['error' => 'Not found'], 404);
-        }
+	/**
+	 * @OA\Get(
+	 *     path="/download/serie/{author-slug}/{serie-slug}",
+	 *     tags={"download"},
+	 *     summary="Download specific serie's books with ZIP",
+	 *     description="Download specific serie, check /series endpoint to get list of slugs",
+	 *     @OA\Parameter(
+	 *         name="author-slug",
+	 *         in="path",
+	 *         description="Slug of author name like 'auel-jean-m' for Jean M. Auel",
+	 *         required=true,
+	 *         example="auel-jean-m",
+	 *         @OA\Schema(
+	 *           type="string",
+	 *           @OA\Items(type="string"),
+	 *         ),
+	 *         style="form"
+	 *     ),
+	 *     @OA\Parameter(
+	 *         name="book-slug",
+	 *         in="path",
+	 *         description="Slug of book name like 'les-enfants-de-la-terre' for Les enfants de la terre",
+	 *         required=true,
+	 *         example="les-enfants-de-la-terre",
+	 *         @OA\Schema(
+	 *           type="string",
+	 *           @OA\Items(type="string"),
+	 *         ),
+	 *         style="form"
+	 *     ),
+	 *     @OA\Response(
+	 *         response=200,
+	 *         description="Successful operation"
+	 *     )
+	 * )
+	 */
+	public function serie(string $author, string $serie)
+	{
+		$epubs = [];
+		$author = Author::whereSlug($author)->firstOrFail();
+		$serie = Serie::with('books')->whereSlug($serie)->firstOrFail();
+		$authorFound = false;
+		foreach ($serie->authors as $key => $authorList) {
+			if ($author->slug === $authorList->slug) {
+				$authorFound = true;
+			}
+		}
+		if (!$authorFound) {
+			return response(['error' => 'Not found'], 404);
+		}
 
-        foreach ($serie->books as $key => $book) {
-            $epub = $book->getMedia('epubs')->first();
-            array_push($epubs, $epub);
-        }
+		foreach ($serie->books as $key => $book) {
+			$epub = $book->getMedia('epubs')->first();
+			array_push($epubs, $epub);
+		}
 
-        $token = Str::random(8);
-        $token = strtolower($token);
-        $author = $serie->author->slug;
-        $dirname = "$author-$serie->slug-$token";
+		$token = Str::random(8);
+		$token = strtolower($token);
+		$author = $serie->meta_author;
+		$dirname = "$author-$serie->slug-$token";
 
-        return MediaStream::create("$dirname.zip")->addMedia($epubs);
-    }
+		return MediaStream::create("$dirname.zip")->addMedia($epubs);
+	}
 
-    /**
-     * @OA\Get(
-     *     path="/download/author/{author-slug}",
-     *     tags={"download"},
-     *     summary="Download specific author's books with ZIP",
-     *     description="Download specific author, check /authors endpoint to get list of slugs",
-     *     @OA\Parameter(
-     *         name="author-slug",
-     *         in="path",
-     *         description="Slug of author name like 'auel-jean-m' for Jean M. Auel",
-     *         required=true,
-     *         example="auel-jean-m",
-     *         @OA\Schema(
-     *           type="string",
-     *           @OA\Items(type="string"),
-     *         ),
-     *         style="form"
-     *     ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Successful operation"
-     *     )
-     * )
-     */
-    public function author(string $author)
-    {
-        $epubs = [];
-        $author = Author::with('books')->whereSlug($author)->firstOrFail();
-        foreach ($author->books as $key => $book) {
-            $epub = $book->getMedia('epubs')->first();
-            array_push($epubs, $epub);
-        }
+	/**
+	 * @OA\Get(
+	 *     path="/download/author/{author-slug}",
+	 *     tags={"download"},
+	 *     summary="Download specific author's books with ZIP",
+	 *     description="Download specific author, check /authors endpoint to get list of slugs",
+	 *     @OA\Parameter(
+	 *         name="author-slug",
+	 *         in="path",
+	 *         description="Slug of author name like 'auel-jean-m' for Jean M. Auel",
+	 *         required=true,
+	 *         example="auel-jean-m",
+	 *         @OA\Schema(
+	 *           type="string",
+	 *           @OA\Items(type="string"),
+	 *         ),
+	 *         style="form"
+	 *     ),
+	 *     @OA\Response(
+	 *         response=200,
+	 *         description="Successful operation"
+	 *     )
+	 * )
+	 */
+	public function author(string $author)
+	{
+		$epubs = [];
+		$author = Author::with('books')->whereSlug($author)->firstOrFail();
+		foreach ($author->books as $key => $book) {
+			$epub = $book->getMedia('epubs')->first();
+			array_push($epubs, $epub);
+		}
 
-        $token = Str::random(8);
-        $token = strtolower($token);
-        $dirname = "$author->slug-$token";
+		$token = Str::random(8);
+		$token = strtolower($token);
+		$dirname = "$author->slug-$token";
 
-        return MediaStream::create("$dirname.zip")->addMedia($epubs);
-    }
+		return MediaStream::create("$dirname.zip")->addMedia($epubs);
+	}
 
-    /**
-     * Old download method.
-     */
-    public function getZip(string $model_name, string $slug)
-    {
-        $modelName = '\App\Models'.'\\'.$model_name;
-        $model = $modelName::with('books')->whereSlug($slug)->firstOrFail();
+	/**
+	 * Old download method.
+	 */
+	public function getZip(string $model_name, string $slug)
+	{
+		$modelName = '\App\Models' . '\\' . $model_name;
+		$model = $modelName::with('books')->whereSlug($slug)->firstOrFail();
 
-        try {
-            $token = Str::random(8);
-            $token = strtolower($token);
-            $dirname = "$model->slug-$token";
-            $path = public_path("storage/$dirname");
-            if (! File::exists($path)) {
-                File::makeDirectory($path);
-            }
+		try {
+			$token = Str::random(8);
+			$token = strtolower($token);
+			$dirname = "$model->slug-$token";
+			$path = public_path("storage/$dirname");
+			if (!File::exists($path)) {
+				File::makeDirectory($path);
+			}
 
-            $downloadList = [];
-            foreach ($model->books->toArray() as $key => $book) {
-                $path = str_replace('storage/', '', $book['epub']['name']);
-                array_push($downloadList, $path);
-            }
-            foreach ($downloadList as $key => $epub) {
-                File::copy(public_path("storage/books/$epub"), public_path("storage/$dirname/$epub"));
-            }
+			$downloadList = [];
+			foreach ($model->books->toArray() as $key => $book) {
+				$path = str_replace('storage/', '', $book['epub']['name']);
+				array_push($downloadList, $path);
+			}
+			foreach ($downloadList as $key => $epub) {
+				File::copy(public_path("storage/books/$epub"), public_path("storage/$dirname/$epub"));
+			}
 
-            $zip = new ZipArchive();
-            $fileName = $dirname.'.zip';
+			$zip = new ZipArchive();
+			$fileName = $dirname . '.zip';
 
-            if (true === $zip->open(public_path('storage/'.$fileName), ZipArchive::CREATE)) {
-                $files = File::files(public_path("storage/$dirname"));
+			if (true === $zip->open(public_path('storage/' . $fileName), ZipArchive::CREATE)) {
+				$files = File::files(public_path("storage/$dirname"));
 
-                foreach ($files as $key => $value) {
-                    $relativeNameInZipFile = basename($value);
-                    $zip->addFile($value, $relativeNameInZipFile);
-                }
+				foreach ($files as $key => $value) {
+					$relativeNameInZipFile = basename($value);
+					$zip->addFile($value, $relativeNameInZipFile);
+				}
 
-                $zip->close();
-            }
+				$zip->close();
+			}
 
-            File::deleteDirectory(public_path("storage/$dirname"));
+			File::deleteDirectory(public_path("storage/$dirname"));
 
-            return public_path("storage/$dirname.zip");
-        } catch (\Throwable $th) {
-            return response()->json('Unexpected error!');
-        }
-    }
+			return public_path("storage/$dirname.zip");
+		} catch (\Throwable $th) {
+			return response()->json('Unexpected error!');
+		}
+	}
 }
