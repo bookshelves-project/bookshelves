ee9d1099cbd8c8b8f215e77805f04f7fa389227b
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-07-18 16:57:49 +0200

    improve color picker

--- app/Providers/MetadataExtractor/MetadataExtractorTools.php
+++ app/Providers/MetadataExtractor/MetadataExtractorTools.php
@@ -359,27 +359,34 @@ class MetadataExtractorTools
     public static function simple_color_thief($img, $default = 'eee')
     {
         if (@exif_imagetype($img)) { // CHECK IF IT IS AN IMAGE
-      $type = getimagesize($img)[2]; // GET TYPE
-      if (1 === $type) { // GIF
-        $image = imagecreatefromgif($img);
-          // IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
-          if (127 == imagecolorsforindex($image, imagecolorstotal($image) - 1)['alpha']) {
-              return 'fff';
-          }
-      } elseif (2 === $type) { // JPG
-          $image = imagecreatefromjpeg($img);
-      } elseif (3 === $type) { // PNG
-          $image = imagecreatefrompng($img);
-          // IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
-          if ((imagecolorat($image, 0, 0) >> 24) & 0x7F === 127) {
-              return 'fff';
-          }
-      } else { // NO CORRECT IMAGE TYPE (GIF, JPG or PNG)
-          return $default;
-      }
+            $type = getimagesize($img)[2]; // GET TYPE
+            if (1 === $type) { // GIF
+                $image = imagecreatefromgif($img);
+                // IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
+                if (127 == imagecolorsforindex($image, imagecolorstotal($image) - 1)['alpha']) {
+                    return 'fff';
+                }
+            } elseif (2 === $type) { // JPG
+                $image = imagecreatefromjpeg($img);
+            } elseif (3 === $type) { // PNG
+                $image = imagecreatefrompng($img);
+                // IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
+                if ((imagecolorat($image, 0, 0) >> 24) & 0x7F === 127) {
+                    return 'fff';
+                }
+            } elseif (18 === $type) { // WEBP
+                $image = imagecreatefromwebp($img);
+                // IF IMAGE IS TRANSPARENT (alpha=127) RETURN fff FOR WHITE
+                if ((imagecolorat($image, 0, 0) >> 24) & 0x7F === 127) {
+                    return 'fff';
+                }
+            } else { // NO CORRECT IMAGE TYPE (GIF, JPG or PNG)
+                return $default;
+            }
         } else { // NOT AN IMAGE
             return $default;
         }
+
         $newImg = imagecreatetruecolor(1, 1); // FIND DOMINANT COLOR
         imagecopyresampled($newImg, $image, 0, 0, 0, 0, 1, 1, imagesx($image), imagesy($image));
         $hexa_color = dechex(imagecolorat($newImg, 0, 0));
