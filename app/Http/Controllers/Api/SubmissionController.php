d5137bbe2617c37a75e932bb5030a51ba853af31
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-10-09 11:43:53 +0200

    add sumbmission honeypot

--- app/Http/Controllers/Api/SubmissionController.php
+++ app/Http/Controllers/Api/SubmissionController.php
@@ -18,8 +18,16 @@ class SubmissionController extends Controller
         $validate = $this->validate($request, [
             'name' => 'required|string',
             'email' => 'required|email:rfc,strict,dns,filter',
+            'honeypot' => 'required|boolean',
             'message' => 'required|string|min:15',
         ]);
+
+        // honeypot fake response
+        if ($validate['honeypot']) {
+            return response()->json([
+                'success' => 'Your mail was sended!',
+            ], 200);
+        }
 
         // Create model
         $submission = Submission::create([
