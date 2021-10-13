d6568e7abc0a966b69ec6189f74800c773f7b71b
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-10-13 12:31:13 +0200

    phpcs fix

--- app/Services/OpdsService.php
+++ app/Services/OpdsService.php
@@ -16,7 +16,7 @@ class OpdsService
         public string $version,
         public EntitiesEnum $entity,
         public string $route,
-        public Collection|Model $data,
+        public Collection | Model $data,
     ) {
     }
 
