d507039589450ada46ad8b67ea55c8df4168127b
Ewilan Rivière <ewilan.riviere@gmail.com>
2021-11-19 22:03:56 +0100

    ⬆️ update node deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require("path");
 
+const { HeadlessUiResolver } = require("unplugin-vue-components/resolvers");
+
 module.exports = {
   resolve: {
     alias: {
       "@": path.resolve("resources/js"),
+      public: path.resolve("public"),
     },
+    modules: [
+      "node_modules",
+      __dirname + "/vendor/spatie/laravel-medialibrary-pro/resources/js",
+    ],
   },
+  plugins: [
+    require("unplugin-vue-components/webpack")({
+      /* options */
+      dirs: ["resources/js/Components", "resources/js/Layouts"],
+      resolvers: [
+        // Resolve HeadlessUI Components
+        HeadlessUiResolver(),
+        // Inertia Resolver
+        (name) => {
+          const prefix = "Inertia";
+          if (name.startsWith(prefix)) {
+            // console.log('Found a Inertia Component! Name:', name)
+            return {
+              importName: name.replace(prefix, ""),
+              path: "@inertiajs/inertia-vue3",
+            };
+          }
+        },
+      ],
+    }),
+  ],
 };
