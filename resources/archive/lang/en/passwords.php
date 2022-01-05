47986851d82e87b0645afbac29d9b7e085471fbc
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-01-05 15:42:54 +0100

    clean

---
+++ resources/archive/lang/en/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'updated' => 'Your password has been updated!',
+    'reset' => 'Your password has been reset!',
+    'sent' => 'If you email exist in our database, we have emailed your password reset link!',
+    'throttled' => 'Please wait before retrying.',
+    'token' => 'This password reset token is invalid.',
+    'user' => "We can't find a user with that email address.",
+];
