0f38d384e77359246657e08116b30e7686d339e5
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-01-06 18:50:45 +0100

    fixes

--- resources/views/components/layouts/documentation.blade.php
+++ resources/views/components/layouts/documentation.blade.php
@@ -13,7 +13,7 @@
                 {{-- <x-layouts.sidebar.header /> --}}
                 <div class="mt-8 min-h-[125vh]">
                     <div class="mt-8">
-                        <div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
+                        <div class="max-w-6xl container px-4 sm:px-6 lg:px-8 mx-auto w-max">
                             {{ $slot }}
                         </div>
                     </div>
