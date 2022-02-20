acf2a93c9a62b4438b153a918f908c9b5da41b7c
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-02-20 14:04:11 +0100

    migrations from L9

--- database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddTwoFactorColumnsToUsersTable extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
@@ -33,4 +32,4 @@ class AddTwoFactorColumnsToUsersTable extends Migration
             $table->dropColumn('two_factor_secret', 'two_factor_recovery_codes');
         });
     }
-}
+};
