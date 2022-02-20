ac433eda44244dc334a8b53c3d58c6c62d680338
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-02-20 13:54:50 +0100

    wip lang, public, base

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\Api\ApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -11,7 +15,8 @@
 |
 */
 
-use App\Http\Controllers\Api\ApiController;
-use Illuminate\Support\Facades\Route;
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 
 Route::get('/', [ApiController::class, 'apiHome'])->name('api.v1.index');
