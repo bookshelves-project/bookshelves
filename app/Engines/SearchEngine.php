7b90b3f64e40b14674469f30275c6230cfc2632d
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-04-07 10:14:46 +0200

    search add opds full

--- app/Engines/SearchEngine.php
+++ app/Engines/SearchEngine.php
@@ -5,16 +5,20 @@ namespace App\Engines;
 use App\Http\Resources\EntityResource;
 use App\Models\Author;
 use App\Models\Book;
+use App\Models\Entity;
 use App\Models\Serie;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Collection;
 
 /**
  * Search Engine with laravel/scout
- * - https://laravel.com/docs/8.x/scout.
+ * - https://laravel.com/docs/9.x/scout.
  */
 class SearchEngine
 {
+    /** @var Collection<int,Entity> */
+    public ?Collection $list = null;
+
     /** @var Collection<int,Author> */
     public ?Collection $authors = null;
     /** @var Collection<int,Serie> */
@@ -36,6 +40,7 @@ class SearchEngine
         public int $count = 0,
         public string $search_type = 'collection',
         public ?string $q = null,
+        public ?bool $sorted = true,
         public ?array $types = [],
     ) {
     }
@@ -43,12 +48,12 @@ class SearchEngine
     /**
      * Create an instance of SearchEngine from query.
      */
-    public static function create(string $q, array|null $types = []): SearchEngine
+    public static function create(string $q, bool $sorted = true, array|null $types = []): SearchEngine
     {
         if (empty($types)) {
             $types = ['books', 'series', 'authors'];
         }
-        $engine = new SearchEngine(q: $q, types: $types);
+        $engine = new SearchEngine(q: $q, sorted: $sorted, types: $types);
 
         return $engine->searchEngine();
     }
@@ -69,13 +74,30 @@ class SearchEngine
         $this->count += $this->series->count();
         $this->count += $this->books->count();
 
-        $this->authors_relevant = EntityResource::collection($this->authors->splice(0, $this->top_limit));
-        $this->series_relevant = EntityResource::collection($this->series->splice(0, $this->top_limit));
-        $this->books_relevant = EntityResource::collection($this->books->splice(0, $this->top_limit));
+        $authors_relevant = $this->authors->splice(0, $this->top_limit);
+        $series_relevant = $this->series->splice(0, $this->top_limit);
+        $books_relevant = $this->books->splice(0, $this->top_limit);
 
-        $this->authors_other = EntityResource::collection($this->authors->splice($this->top_limit, $this->max_limit));
-        $this->series_other = EntityResource::collection($this->series->splice($this->top_limit, $this->max_limit));
-        $this->books_other = EntityResource::collection($this->books->splice($this->top_limit, $this->max_limit));
+        $authors_other = $this->authors->splice($this->top_limit, $this->max_limit);
+        $series_other = $this->series->splice($this->top_limit, $this->max_limit);
+        $books_other = $this->books->splice($this->top_limit, $this->max_limit);
+        if ($this->sorted) {
+            $this->authors_relevant = EntityResource::collection($authors_relevant);
+            $this->series_relevant = EntityResource::collection($series_relevant);
+            $this->books_relevant = EntityResource::collection($books_relevant);
+
+            $this->authors_other = EntityResource::collection($authors_other);
+            $this->series_other = EntityResource::collection($series_other);
+            $this->books_other = EntityResource::collection($books_other);
+        } else {
+            $this->list = collect();
+            $this->list->push(...$authors_relevant);
+            $this->list->push(...$series_relevant);
+            $this->list->push(...$books_relevant);
+            $this->list->push(...$authors_other);
+            $this->list->push(...$series_other);
+            $this->list->push(...$books_other);
+        }
 
         return $this;
     }
@@ -88,12 +110,12 @@ class SearchEngine
         $scout_search = 'collection' !== $this->search_type;
         if (in_array('authors', $this->types)) {
             $this->authors = $scout_search ?
-                    Author::search($this->q)
-                        ->get()
-                    : Author::whereLike(['name', 'firstname', 'lastname'], $this->q)
-                        ->with('media')
-                        ->get()
-                    ;
+                Author::search($this->q)
+                    ->get()
+                : Author::whereLike(['name', 'firstname', 'lastname'], $this->q)
+                    ->with('media')
+                    ->get()
+                ;
         }
         if (in_array('series', $this->types)) {
             $this->series = $scout_search ?
