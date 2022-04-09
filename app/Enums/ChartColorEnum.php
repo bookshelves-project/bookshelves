8ed7ba661cd0372851aa2329cc97d5c79aa8291b
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-04-09 09:58:46 +0200

    admin add charts colors

--- app/Enums/ChartColorEnum.php
+++ app/Enums/ChartColorEnum.php
@@ -8,13 +8,12 @@ enum ChartColorEnum: string
 {
     use EnumMethods;
 
-    case color_e2e0ff = '#e2e0ff';
-    case color_c4c1ff = '#c4c1ff';
-    case color_a7a1ff = '#a7a1ff';
-    case color_8982ff = '#8982ff';
-    case color_6c63ff = '#6c63ff';
-    case color_564fcc = '#564fcc';
-    case color_413b99 = '#413b99';
-    case color_2b2866 = '#2b2866';
-    case color_161433 = '#161433';
+    case color_003f5c = '#003f5c';
+    case color_2f4b7c = '#2f4b7c';
+    case color_665191 = '#665191';
+    case color_a05195 = '#a05195';
+    case color_d45087 = '#d45087';
+    case color_f95d6a = '#f95d6a';
+    case color_ff7c43 = '#ff7c43';
+    case color_ffa600 = '#ffa600';
 }
