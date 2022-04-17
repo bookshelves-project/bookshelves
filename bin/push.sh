cc37d11b63f75f01b933d0260abab5a5ce2c358b
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-04-17 10:44:34 +0200

    bin improve with remote

--- bin/push.sh
+++ bin/push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# git remote add framagit git@github.com:bookshelves-project/bookshelves-back.git
+# git remote add framagit git@framagit.org:bookshelves-project/bookshelves-back.git
+
 git push
 git push github
 git push framagit
