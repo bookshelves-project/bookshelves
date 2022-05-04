d45064cafeddc979e483b258c693d795a8ce536d
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-05-04 15:14:15 +0200

    readme, script

--- scripts/clean.ps1
+++ scripts/clean.ps1
@@ -1,1 +1,5 @@
+rm ./.phpstan -r -fo
+rm ./.scribe -r -fo
 rm ./node_modules -r -fo
+rm ./public/assets/dist -r -fo
+rm ./vendor -r -fo
---
+++ scripts/clean.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+rm -rf ./.phpstan
+rm -rf ./.scribe
+rm -rf ./node_modules
+rm -rf ./public/assets/dist
+rm -rf ./vendor
