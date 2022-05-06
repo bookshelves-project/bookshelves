c46c969dc39519b43dbfdec51ff4faa06ea98f47
Ewilan Rivière <ewilan.riviere@gmail.com>
2022-05-06 22:23:06 +0200

    catalog fix

--- resources/front/pages/catalog/books/_slug.blade.php
+++ resources/front/pages/catalog/books/_slug.blade.php
@@ -30,6 +30,10 @@
     {!! $book->description !!}
 </x-catalog.table>
 
+<a href="{{ $book->download->url }}">download</a>
+<a href="{{ $book->download->url }}" target="_blank" rel="noopener noreferrer">download</a>
+<a href="{{ $book->download->url }}" target="_blank" rel="noopener noreferrer" download="{{ $book->download->name }}">{{ $book->download->url }}</a>
+
 @isset($book->download)
 <x-catalog.button url="{{ $book->download->url }}" download>
     Download
