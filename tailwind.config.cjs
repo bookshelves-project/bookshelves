c6e03a66e945d6d36f4e5d411ede35e78f175b80
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-05-09 21:57:04 +0200

    deps

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -60,6 +60,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp'),
   ],
 }
