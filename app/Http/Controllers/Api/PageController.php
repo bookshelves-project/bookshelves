762065b9a552ac0c8f7a580aa024eb617a491aae
Ewilan Rivière <ewilan.riviere@gmail.com>
2023-07-06 19:40:47 +0200

    fix

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -5,13 +5,9 @@ namespace App\Http\Controllers\Api;
 use App\Http\Resources\Page\PageCollection;
 use App\Http\Resources\Page\PageResource;
 use App\Models\Page;
-use Spatie\RouteAttributes\Attributes\Get;
-use Spatie\RouteAttributes\Attributes\Prefix;
 
-#[Prefix('pages')]
 class PageController extends Controller
 {
-    #[Get('/', name: 'pages.index')]
     public function index()
     {
         $pages = Page::all();
@@ -19,7 +15,6 @@ class PageController extends Controller
         return PageCollection::collection($pages);
     }
 
-    #[Get('/{page_slug}', name: 'pages.show')]
     public function show(Page $page)
     {
         return PageResource::make($page);
