86d8bcbd93027c1b9234eea8b654102a9b65622b
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-01-22 18:10:05 +0100

    Add ScoutFreshCommand and SetupCommand to schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,8 +2,11 @@
 
 namespace App\Console;
 
+use App\Console\Commands\Bookshelves\SetupCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
+use Kiwilan\Steward\Commands\Scout\ScoutFreshCommand;
 
 class Kernel extends ConsoleKernel
 {
@@ -12,7 +15,25 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(ScoutFreshCommand::class)
+            ->at('01:00')
+            ->mondays()
+            ->onSuccess(function () {
+                Log::info('ScoutFreshCommand executed successfully');
+            })
+            ->onFailure(function () {
+                Log::error('ScoutFreshCommand failed');
+            });
+
+        $schedule->command(SetupCommand::class)
+            ->at('02:00')
+            ->daily()
+            ->onSuccess(function () {
+                Log::info('SetupCommand executed successfully');
+            })
+            ->onFailure(function () {
+                Log::error('SetupCommand failed');
+            });
     }
 
     /**
