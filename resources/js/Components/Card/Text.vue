6511a9218f4de95939ef9d8e97c5245ac9de5a7f
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-03-14 18:19:58 +0100

    Refactor text truncation logic in Card/Text component

--- resources/js/Components/Card/Text.vue
+++ resources/js/Components/Card/Text.vue
@@ -3,13 +3,18 @@ const props = defineProps<{
   text?: string
 }>()
 
+const limit = 600
+const expandable = ref(false)
 const excerpt = ref<string>()
 const expanded = ref(false)
 
+if (props.text && props.text.length > limit)
+  expandable.value = true
+
 function spliceText() {
   if (props.text) {
-    excerpt.value = props.text?.slice(0, 600)
-    if (props.text.length > 1000)
+    excerpt.value = props.text?.slice(0, limit)
+    if (props.text.length > limit)
       excerpt.value += '...'
   }
 }
@@ -32,6 +37,7 @@ function readMore() {
       {{ excerpt }}
     </p>
     <button
+      v-if="expandable"
       class="mt-2 text-purple-400 underline hover:text-purple-300"
       @click="readMore"
     >
