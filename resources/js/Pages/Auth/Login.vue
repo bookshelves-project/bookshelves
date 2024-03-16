8f0a05446b155d7e7c84eb8f6d71c5d74e42e375
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-03-16 13:43:53 +0100

    Refactor login page layout and styling

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -30,98 +30,98 @@ function submit() {
 </script>
 
 <template>
-  <AppMeta
+  <App
     title="Log in"
     description="Log in to your account"
-  />
+  >
+    <AuthenticationCard>
+      <template #logo>
+        <AuthenticationCardLogo />
+      </template>
 
-  <AuthenticationCard>
-    <template #logo>
-      <AuthenticationCardLogo />
-    </template>
-
-    <div
-      v-if="status"
-      class="mb-4 font-medium text-sm text-green-600"
-    >
-      {{ status }}
-    </div>
-
-    <form @submit.prevent="submit">
-      <div>
-        <InputLabel
-          for="email"
-          value="Email"
-        />
-        <TextInput
-          id="email"
-          v-model="form.email"
-          type="email"
-          class="mt-1 block w-full"
-          required
-          autofocus
-          autocomplete="username"
-        />
-        <InputError
-          class="mt-2"
-          :message="form.errors.email"
-        />
+      <div
+        v-if="status"
+        class="mb-4 font-medium text-sm text-green-600"
+      >
+        {{ status }}
       </div>
 
-      <div class="mt-4">
-        <InputLabel
-          for="password"
-          value="Password"
-        />
-        <TextInput
-          id="password"
-          v-model="form.password"
-          type="password"
-          class="mt-1 block w-full"
-          required
-          autocomplete="current-password"
-        />
-        <InputError
-          class="mt-2"
-          :message="form.errors.password"
-        />
-      </div>
-
-      <div class="block mt-4">
-        <label class="flex items-center">
-          <Checkbox
-            v-model:checked="form.remember"
-            name="remember"
+      <form @submit.prevent="submit">
+        <div>
+          <InputLabel
+            for="email"
+            value="Email"
           />
-          <span class="ms-2 text-sm text-gray-600">Remember me</span>
-        </label>
-      </div>
-
-      <div class="flex items-center justify-end mt-4">
-        <div class="flex items-center space-x-3">
-          <Link
-            href="/"
-            class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Back
-          </Link>
-          <Link
-            v-if="canResetPassword"
-            :href="route('password.request')"
-            class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Forgot your password?
-          </Link>
+          <TextInput
+            id="email"
+            v-model="form.email"
+            type="email"
+            class="mt-1 block w-full"
+            required
+            autofocus
+            autocomplete="username"
+          />
+          <InputError
+            class="mt-2"
+            :message="form.errors.email"
+          />
         </div>
 
-        <PrimaryButton
-          class="ms-4"
-          :class="{ 'opacity-25': form.processing }"
-          :disabled="form.processing"
-        >
-          Log in
-        </PrimaryButton>
-      </div>
-    </form>
-  </AuthenticationCard>
+        <div class="mt-4">
+          <InputLabel
+            for="password"
+            value="Password"
+          />
+          <TextInput
+            id="password"
+            v-model="form.password"
+            type="password"
+            class="mt-1 block w-full"
+            required
+            autocomplete="current-password"
+          />
+          <InputError
+            class="mt-2"
+            :message="form.errors.password"
+          />
+        </div>
+
+        <div class="block mt-4">
+          <label class="flex items-center">
+            <Checkbox
+              v-model:checked="form.remember"
+              name="remember"
+            />
+            <span class="ms-2 text-sm text-gray-600">Remember me</span>
+          </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+          <div class="flex items-center space-x-3">
+            <Link
+              href="/"
+              class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Back
+            </Link>
+            <Link
+              v-if="canResetPassword"
+              :href="route('password.request')"
+              class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Forgot your password?
+            </Link>
+          </div>
+
+          <PrimaryButton
+            class="ms-4"
+            :class="{ 'opacity-25': form.processing }"
+            :disabled="form.processing"
+          >
+            Log in
+          </PrimaryButton>
+        </div>
+      </form>
+    </AuthenticationCard>
+  </App>
 </template>
