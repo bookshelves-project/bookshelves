fd7f7740996bfcecc1b610554bdc2a85e2c4a580
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-03-16 12:48:02 +0100

    Add unplugin-auto-import, unplugin-svg-transformer, and unplugin-vue-components dependencies

---
+++ resources/js/Components/App/Meta.vue
@@ -0,0 +1,98 @@
+<script lang="ts" setup>
+import { Head } from '@inertiajs/vue3'
+
+export interface Props {
+  title?: string
+  description?: string
+  image?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: 'Bookshelves',
+  description: undefined,
+  image: `${window.location.origin}/default.jpg`,
+})
+
+const currentUrl = computed(() => window.location.href)
+const domain = computed(() => window.location.host)
+
+const titleContent = computed(() => {
+  if (!props.title)
+    return 'Bookshelves'
+
+  if (props.title.length > 60)
+    return `${props.title.substring(0, 60)}...`
+
+  return props.title
+})
+const descriptionContent = computed(() => {
+  if (!props.description)
+    return 'For people with eReaders, download eBooks and reading in complete tranquility, your digital library that goes everywhere with you.'
+
+  if (props.description.length > 160)
+    return `${props.description.substring(0, 160)}...`
+
+  return props.description
+})
+</script>
+
+<template>
+  <Head :title="props.title">
+    <meta
+      head-key="description"
+      name="description"
+      :content="descriptionContent"
+    >
+
+    <meta
+      head-key="og:url"
+      property="og:url"
+      :content="currentUrl"
+    >
+    <meta
+      head-key="og:type"
+      property="og:type"
+      content="website"
+    >
+    <meta
+      head-key="og:title"
+      property="og:title"
+      :content="titleContent"
+    >
+    <meta
+      head-key="og:description"
+      property="og:description"
+      :content="descriptionContent"
+    >
+    <meta
+      head-key="og:image"
+      property="og:image"
+      :content="image"
+    >
+
+    <meta
+      name="twitter:card"
+      content="summary_large_image"
+    >
+    <meta
+      property="twitter:domain"
+      :content="domain"
+    >
+    <meta
+      property="twitter:url"
+      :content="currentUrl"
+    >
+    <meta
+      name="twitter:title"
+      :content="titleContent"
+    >
+    <meta
+      name="twitter:description"
+      :content="descriptionContent"
+    >
+    <meta
+      name="twitter:image"
+      :content="image"
+    >
+  </Head>
+</template>
