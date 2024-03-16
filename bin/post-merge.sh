621080e441cdda5bc6d18bb0a02251020757ec56
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-03-16 10:39:28 +0100

    Add Inertia SSR start and stop commands to post-merge script

--- bin/post-merge.sh
+++ bin/post-merge.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # supervisorctl stop bookshelves-worker
+php artisan inertia:stop-ssr
 composer i
 php artisan migrate --force
 pnpm i
@@ -9,4 +10,5 @@ php artisan optimize:fresh
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
+php artisan inertia:start-ssr &
 # supervisorctl start bookshelves-worker
