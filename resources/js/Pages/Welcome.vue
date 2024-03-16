f2708a73c3efa535927b53cecce5fc7de5fcdc56
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-03-16 13:34:30 +0100

    Update AppMeta in Welcome.vue

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -27,7 +27,10 @@ function handleImageError() {
 </script>
 
 <template>
-  <AppMeta title="Welcome" />
+  <AppMeta
+    title="Welcome"
+    description="Welcome on this app"
+  />
   <div class="bg-gray-50 text-black/50 dark:bg-black dark:text-white/50">
     <img
       id="background"
