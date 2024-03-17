f7fdf366f532b476e06946cf8267a6aa385985f2
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-03-17 18:34:52 +0100

    Update Home.vue template

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -3,26 +3,7 @@
 </script>
 
 <template>
-  <App
-    title="Welcome on Bookshelves"
-    icon="home"
-  >
-    <div class="pt-10 space-y-6">
-      <!-- <SwiperHome
-        endpoint="api.books.latest"
-        title="Books recently added"
-        type="book"
-      /> -->
-      <!-- <SwiperHome
-        endpoint="api.books.released"
-        title="Books recently released"
-        type="book"
-      /> -->
-      <!-- <SwiperHome
-        endpoint="api.series.latest"
-        title="Series recently added"
-        type="serie"
-      /> -->
-    </div>
-  </App>
+  <div>
+    bookshelves
+  </div>
 </template>
