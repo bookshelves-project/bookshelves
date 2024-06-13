ba5c047602975cfe1c7485f90669050ddf27cd9f
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-06-13 17:02:37 +0200

    chore: Adjust EventFlag component position in EventFlag.vue

--- resources/js/Components/EventFlag.vue
+++ resources/js/Components/EventFlag.vue
@@ -11,6 +11,6 @@ display.value = months.includes(currentMonth)
     v-if="display"
     src="/images/lgbti.svg"
     alt="LGBTI flag"
-    class="absolute w-14 rotate-12 top-4 -right-5 rounded-md"
+    class="absolute w-14 rotate-12 top-4 -right-2 rounded-md"
   >
 </template>
