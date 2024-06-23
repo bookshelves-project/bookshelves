188f7900e28e4c3164e9e887af7ad27fdc5c62c9
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-06-23 08:40:43 +0200

    refactor: Remove unused code in SerieController

--- app/Http/Controllers/App/SerieController.php
+++ app/Http/Controllers/App/SerieController.php
@@ -5,21 +5,12 @@ namespace App\Http\Controllers\App;
 use App\Http\Controllers\Controller;
 use App\Models\Library;
 use App\Models\Serie;
-use Illuminate\Http\Request;
 use Spatie\RouteAttributes\Attributes\Get;
 use Spatie\RouteAttributes\Attributes\Prefix;
 
 #[Prefix('series')]
 class SerieController extends Controller
 {
-    #[Get('/', name: 'series.index')]
-    public function index(Request $request)
-    {
-        return $this->getQueryForSeries($request, Serie::whereIsBook(), 'Book series', [
-            ['label' => 'Book series', 'route' => ['name' => 'series.books.index']],
-        ]);
-    }
-
     #[Get('/{library:slug}/{serie:slug}', name: 'series.show')]
     public function show(Library $library, string $serie)
     {
