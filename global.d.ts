18e318577545125d2b423653627d1016d4acf0a4
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-07-12 11:06:06 +0200

    refactor: Update global.d.ts and Components
    - Remove unused import and code in global.d.ts
    - Add null check for window object in Carousel.vue and Tabs.vue

--- global.d.ts
+++ global.d.ts
@@ -1,13 +1,3 @@
 /// <reference types="vite/client" />
 
-// import type { Axios } from 'axios'
-
-declare global {
-  interface Window {
-    // axios: Axios
-  }
-}
-
-// window.axios = window.axios || {}
-
 export {}
