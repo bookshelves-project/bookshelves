0613c7296f31ec10c456df9182d70bdf8d9b1860
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-08-02 18:33:49 +0200

    refactor: Update TagController to improve performance and add series support
    The TagController has been updated to improve performance and add support for series. The `show` method now includes eager loading of related models and uses the `whereTagIs` scope to filter books and series by the specified tag. This change enhances the efficiency of retrieving and displaying tag-related content.

--- resources/js/Pages/Tags/Show.vue
+++ resources/js/Pages/Tags/Show.vue
@@ -1,6 +1,10 @@
 <script lang="ts" setup>
 defineProps<{
-  title: string
+  tag: App.Models.Tag
+  query: App.Paginate<App.Models.Book>
+  breadcrumbs?: any[]
+  title?: string
+  series?: boolean
 }>()
 </script>
 
@@ -9,8 +13,50 @@ defineProps<{
     :title="title"
     icon="tag"
   >
-    <div class="main-container">
-      <p>Soon!</p>
-    </div>
+    <ListingTabs
+      :links="[
+        { label: 'Books', href: $route('tags.show', { tag: tag.slug }) },
+        { label: 'Series', href: $route('tags.show.series', { tag: tag.slug }) },
+      ]"
+    />
+    <Listing
+      :query="query"
+      :sortable="series
+        ? [
+          { label: 'Title', value: 'title' },
+          { label: 'Added at', value: 'created_at' },
+        ]
+        : [
+          { label: 'Series\'s Title', value: 'slug' },
+          { label: 'Title', value: 'title' },
+          { label: 'Release date', value: 'released_on' },
+          { label: 'Added at', value: 'added_at' },
+        ]"
+      :filterable="[
+        { label: 'English', value: 'en' },
+        { label: 'French', value: 'fr' },
+      ]"
+    >
+      <template
+        v-if="breadcrumbs"
+        #breadcrumbs
+      >
+        <Breadcrumbs :breadcrumbs="breadcrumbs" />
+      </template>
+      <template v-if="series">
+        <CardSerie
+          v-for="serie in query.data"
+          :key="serie.id"
+          :serie="serie"
+        />
+      </template>
+      <template v-else>
+        <CardBook
+          v-for="book in query.data"
+          :key="book.id"
+          :book="book"
+        />
+      </template>
+    </Listing>
   </App>
 </template>
