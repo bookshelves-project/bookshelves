488ce6ca935392d2767cc14ffe6474ece437e360
Ewilan Rivière <ewilan.riviere@gmail.com>
2024-10-23 12:49:18 +0200

    refactor: Update Foliate library dependencies and code files

--- resources/js/Pages/Reader.vue
+++ resources/js/Pages/Reader.vue
@@ -1,13 +1,13 @@
 <script lang="ts" setup>
-import { useFoliate } from '@/Composables/useFoliate'
+// import { useFoliate } from '@/Composables/useFoliate'
 
-const { book } = defineProps<{
-  book: App.Models.Book
-}>()
+// const { book } = defineProps<{
+//   book: App.Models.Book
+// }>()
 
-onMounted(async () => {
-  const { ebook } = await useFoliate(book.download_link)
-})
+// onMounted(async () => {
+//   const { ebook } = await useFoliate(book.download_link)
+// })
 // console.log(ebook.value)
 // console.log(ebook.value?.rendition)
 </script>
