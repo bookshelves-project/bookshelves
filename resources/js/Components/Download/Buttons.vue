cd870af665fcce84970c9cf8907f199b74ae7a48
Ewilan Rivière <ewilan.riviere@gmail.com>
2025-05-12 08:39:05 +0200

    Refactor download buttons in Buttons.vue for clarity and update button labels

--- resources/js/Components/Download/Buttons.vue
+++ resources/js/Components/Download/Buttons.vue
@@ -24,36 +24,41 @@ function notification() {
 </script>
 
 <template>
-  <div class="block md:flex items-center space-y-3 md:space-y-0 md:space-x-3">
-    <template v-if="useNitro">
-      <AppButton
-        v-if="useNitro"
-        icon="download"
-        @click="[save(model, type), notification()]"
-      >
-        <span>Download</span>
-        <span class="ml-1">({{ size }})</span>
-      </AppButton>
-      <AppButton
-        :href="url"
-        icon="download"
-        color="secondary"
-        download
-        @click="notification()"
-      >
-        <span>Download (legacy)</span>
-      </AppButton>
-    </template>
-    <template v-else>
-      <AppButton
-        :href="url"
-        icon="download"
-        download
-        @click="notification()"
-      >
-        <span>Download</span>
-        <span class="ml-1">({{ size }})</span>
-      </AppButton>
-    </template>
+  <div>
+    <div class="block md:flex items-center space-y-3 md:space-y-0 md:space-x-3">
+      <template v-if="useNitro">
+        <AppButton
+          v-if="useNitro"
+          icon="download"
+          @click="[save(model, type), notification()]"
+        >
+          <span>Download</span>
+          <span class="ml-1">({{ size }})</span>
+        </AppButton>
+        <AppButton
+          :href="url"
+          icon="download"
+          color="secondary"
+          download
+          @click="notification()"
+        >
+          <span>Download (slow)</span>
+        </AppButton>
+      </template>
+      <template v-else>
+        <AppButton
+          :href="url"
+          icon="download"
+          download
+          @click="notification()"
+        >
+          <span>Download</span>
+          <span class="ml-1">({{ size }})</span>
+        </AppButton>
+      </template>
+    </div>
+    <p class="mt-3 italic text-xs text-gray-300 prose">
+      First button with file size uses Nitro, a side service that allows you to download files faster. The second button is a direct link to the file, but it may be slower.
+    </p>
   </div>
 </template>
