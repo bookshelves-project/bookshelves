b2cafcd12b85dc34a4895c769344d352053dd93e
Ewilan Rivière <ewilan.riviere@gmail.com>
2025-08-15 15:51:12 +0200

    feat: Reduce maxProcesses for production supervisor to improve resource management

--- config/horizon.php
+++ config/horizon.php
@@ -198,7 +198,7 @@ return [
     'environments' => [
         'production' => [
             'supervisor-1' => [
-                'maxProcesses' => 10,
+                'maxProcesses' => 1,
                 'balanceMaxShift' => 1,
                 'balanceCooldown' => 3,
             ],
