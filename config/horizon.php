0495b3e083f2c28a9da5f625a5f1f8305ab6072e
Ewilan Rivière <ewilan.riviere@gmail.com>
2025-08-16 20:09:24 +0200

    refactor: Decrease maxProcesses in production environment configuration for Horizon

--- config/horizon.php
+++ config/horizon.php
@@ -198,7 +198,7 @@ return [
     'environments' => [
         'production' => [
             'supervisor-1' => [
-                'maxProcesses' => 3,
+                'maxProcesses' => 2,
                 'balanceMaxShift' => 1,
                 'balanceCooldown' => 3,
             ],
