50b598725cf8696d3425f75afc8a2fe17235b9a3
Ewilan Rivière <ewilan.riviere@gmail.com>
2025-08-16 17:12:16 +0200

    fix: Update fusionAudiobook method to correctly skip single track cases

--- app/Jobs/Book/BookJob.php
+++ app/Jobs/Book/BookJob.php
@@ -149,7 +149,7 @@ class BookJob implements ShouldQueue
         // {"track_number":"1/4","track_number_int":1,"after_slash":"4"}
         // {"track_number":"2/2","track_number_int":2,"after_slash":"2"}
         // skip if only one track
-        if ($track_number_int < 2 && $after_slash === null && $track_number === '1/1') {
+        if ($track_number_int < 2 && $after_slash === null || $track_number === '1/1') {
             return;
         }
 
