12abbbe2479c2880b0a13df6eddef9ca8b8f79a5
Ewilan Rivière <ewilan.riviere@gmail.com>
2025-08-16 11:17:44 +0200

    fix: Remove nullable constraint from book_id in audiobook_tracks migration

--- database/migrations/2024_01_23_093314_create_audiobook_tracks_table.php
+++ database/migrations/2024_01_23_093314_create_audiobook_tracks_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('slug')->nullable();
 
             $table->foreignUlid('book_id')
-                ->nullable()
                 ->constrained('books')
                 ->cascadeOnDelete();
 
