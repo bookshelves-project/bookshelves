a08b72928d9b84af2ed608a8d4b615c2a8ef470c
Ewilan Rivière <ewilan.riviere@gmail.com>
2025-08-17 13:48:43 +0200

    refactor: Re-add URL facade and enforce HTTPS scheme in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
+
         \Illuminate\Database\Eloquent\Model::preventLazyLoading(! $this->app->environment('production'));
 
         \Opcodes\LogViewer\Facades\LogViewer::auth(function (Request $request) {
