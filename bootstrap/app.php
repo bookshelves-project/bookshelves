f5a4476ecced2c28d30e24fd4116f3c5f96ddc86
Ewilan Rivière <ewilan.riviere@gmail.com>
2025-08-18 09:15:52 +0200

    fix: Update trustProxies middleware to include additional headers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,7 +18,11 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->trustProxies(at: [
             '*',
-        ]);
+        ], headers: \Illuminate\Http\Request::HEADER_X_FORWARDED_FOR
+            | \Illuminate\Http\Request::HEADER_X_FORWARDED_HOST
+            | \Illuminate\Http\Request::HEADER_X_FORWARDED_PORT
+            | \Illuminate\Http\Request::HEADER_X_FORWARDED_PROTO
+            | \Illuminate\Http\Request::HEADER_X_FORWARDED_AWS_ELB);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         if (config('app.env') === 'production') {
