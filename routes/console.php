021b522530166167aafd3ea22faec4838791bd28
Ewilan Rivière <ewilan.riviere@gmail.com>
2025-09-01 09:27:02 +0200

    feat: add scheduling for ScoutFreshCommand with success and failure logging

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use App\Console\Commands\Bookshelves\AnalyzeCommand;
 use Illuminate\Support\Facades\Schedule;
 use Kiwilan\LaravelNotifier\Facades\Journal;
+use Kiwilan\Steward\Commands\Scout\ScoutFreshCommand;
 
 Schedule::command(AnalyzeCommand::class)
     ->at('02:00')
@@ -12,4 +13,14 @@ Schedule::command(AnalyzeCommand::class)
     })
     ->onFailure(function () {
         Journal::error('AnalyzeCommand failed')->toDatabase();
+    });
+
+Schedule::command(ScoutFreshCommand::class)
+    ->at('06:00')
+    ->daily()
+    ->onSuccess(function () {
+        Journal::info('ScoutFreshCommand executed successfully');
+    })
+    ->onFailure(function () {
+        Journal::error('ScoutFreshCommand failed')->toDatabase();
     });
