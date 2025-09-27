05db34766f64da896613b19eeedb229956523282
Ewilan Rivière <ewilan.riviere@gmail.com>
2025-09-27 13:00:10 +0200

    fix link

--- resources/js/Components/Show/Media.vue
+++ resources/js/Components/Show/Media.vue
@@ -17,23 +17,17 @@ const tagsList = computed(() => {
 <template>
   <section class="card lg:mt-10">
     <div class="lg:grid grid-cols-6 gap-10 space-y-6 lg:space-y-0">
-      <div
-        :class="{
-          'aspect-square lg:aspect-poster md:h-64 lg:h-[unset]': !square,
-          'aspect-square': square,
-        }"
-        class="book-shadow block overflow-hidden bg-gray-900 rounded-md col-span-2"
-      >
-        <a
-          :href="cover"
+      <div :class="{
+        'aspect-square lg:aspect-poster md:h-64 lg:h-[unset]': !square,
+        'aspect-square': square,
+      }"
+        class="book-shadow block overflow-hidden bg-gray-900 rounded-md col-span-2">
+        <a :href="cover"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          <AppImg
-            class="object-cover h-full w-full"
+          rel="noopener noreferrer">
+          <AppImg class="object-cover h-full w-full"
             :src="cover"
-            :alt="title"
-          />
+            :alt="title" />
         </a>
       </div>
       <section class="col-span-4">
@@ -44,10 +38,6 @@ const tagsList = computed(() => {
             <slot name="eyebrow" />
           </div>
           <h2 class="mt-1 text-3xl font-semibold relative">
-            <a
-              href="/"
-              class="absolute inset-0"
-            />
             <slot name="title" />
             {{ title }}
           </h2>
@@ -55,47 +45,33 @@ const tagsList = computed(() => {
             <slot name="undertitle" />
             {{ undertitle }}
           </div>
-          <div
-            v-if="propertiesList?.length || badgesList?.length"
-            class="mt-4 text-sm"
-          >
-            <div
-              v-if="propertiesList"
-              class="flex flex-wrap gap-3"
-            >
-              <span
-                v-for="property in propertiesList"
-                :key="property"
-              >
+          <div v-if="propertiesList?.length || badgesList?.length"
+            class="mt-4 text-sm">
+            <div v-if="propertiesList"
+              class="flex flex-wrap gap-3">
+              <span v-for="property in propertiesList"
+                :key="property">
                 {{ property }}
               </span>
             </div>
-            <div
-              v-if="badgesList"
-              class="mt-3 flex flex-wrap gap-3"
-            >
-              <AppBadge
-                v-for="badge in badgesList"
-                :key="badge"
-              >
+            <div v-if="badgesList"
+              class="mt-3 flex flex-wrap gap-3">
+              <AppBadge v-for="badge in badgesList"
+                :key="badge">
                 {{ badge }}
               </AppBadge>
             </div>
           </div>
-          <div
-            v-if="tagsList?.length"
-            class="mt-3 text-sm"
-          >
+          <div v-if="tagsList?.length"
+            class="mt-3 text-sm">
             <ShowTags :tags="tagsList" />
           </div>
         </div>
         <div class="mb-6 mt-8 flex items-center space-x-3">
           <slot name="buttons" />
         </div>
-        <div
-          v-if="overview"
-          class="mt-3"
-        >
+        <div v-if="overview"
+          class="mt-3">
           <CardText :text="overview" />
         </div>
         <div class="mt-6">
